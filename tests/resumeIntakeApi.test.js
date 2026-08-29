3012dc5fca619cbdaafdd1cdfb15678b064934e0
Luis Ochoa <lochoa66x@gmail.com>
2026-08-29 11:29:38 -0400

    chore: keep API tests out of deployment

--- tests/resumeIntakeApi.test.js
+++ tests/resumeIntakeApi.test.js
@@ -1,7 +1,7 @@
 import assert from "node:assert/strict";
 import test from "node:test";
 
-import { createResumeIntakeHandler } from "./resume-intake.js";
+import { createResumeIntakeHandler } from "../api/resume-intake.js";
 
 function responseRecorder() {
   return {
